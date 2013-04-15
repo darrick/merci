663d4c31708a82dcbc815ddc34084bf87d3656f7
Darrick Servis <darrick@davismedia.org>
2013-04-14 19:32:04 -0700

    Added selection handler

--- plugins/selection/EntityReference_SelectionHandler_Merci.class.php
+++ plugins/selection/EntityReference_SelectionHandler_Merci.class.php
@@ -6,54 +6,23 @@
  * The generic base implementation has a variety of overrides to workaround
  * core's largely deficient entity handling.
  */
-class EntityReference_SelectionHandler_Merci implements EntityReference_SelectionHandler {
+class EntityReference_SelectionHandler_Merci extends EntityReference_SelectionHandler_Generic {
 
-  /**
-   * Implements EntityReferenceHandler::getInstance().
-   */
   public static function getInstance($field, $instance = NULL, $entity_type = NULL, $entity = NULL) {
-    $target_entity_type = $field['settings']['target_type'];
-
-    // Check if the entity type does exist and has a base table.
-    $entity_info = entity_get_info($target_entity_type);
-    if (empty($entity_info['base table'])) {
-      return EntityReference_SelectionHandler_Broken::getInstance($field, $instance);
-    }
-
-    // TODO may not need to call a subclass by entity.
-    if (class_exists($class_name = 'EntityReference_SelectionHandler_Merci_' . $target_entity_type)) {
-      return new $class_name($field, $instance, $entity_type, $entity);
-    }
-    else {
-      return new EntityReference_SelectionHandler_Merci($field, $instance, $entity_type, $entity);
-    }
+    dpm($field);
+    dpm($instance);
+    dpm($entity_type);
+    dpm($entity);
+    return new EntityReference_SelectionHandler_Merci($field, $instance, $entity_type, $entity);
   }
-
-  protected function __construct($field, $instance = NULL, $entity_type = NULL, $entity = NULL) {
-    $this->field = $field;
-    $this->instance = $instance;
-    $this->entity_type = $entity_type;
-    $this->entity = $entity;
-  }
-
-  /**
-   * Implements EntityReferenceHandler::settingsForm().
-   */
-  public static function settingsForm($field, $instance) {
-
-    $form['merci'] = array(
-      '#markup' => t('This handler has no options')
-    );
-
-
-    return $form;
-  }
-
   /**
    * Implements EntityReferenceHandler::getReferencableEntities().
    */
   public function getReferencableEntities($match = NULL, $match_operator = 'CONTAINS', $limit = 0) {
+    $options = parent::getReferencableEntities($match, $match_operator, $limit);
     $options = array();
+    $entity_type = $this->field['settings']['target_type'];
+    $field_item_name = $this->field['field_name'];
 
     $query = $this->buildEntityFieldQuery($match, $match_operator);
     if ($limit > 0) {
@@ -61,7 +30,24 @@ class EntityReference_SelectionHandler_Merci implements EntityReference_Selectio
     }
 
     $results = $query->execute();
+    dpm($results);
+    $items = array();
+    foreach($results[$entity_type] as $id => $target) {
+      $items[] = array('target_id' => $id);
+    }
 
+    extract(get_object_vars($this));
+    $langcode = $entity->language;
+    dpm($items);
+
+    $errors = array();
+    merci_api_validate_reference($entity_type, $entity, $field, $instance, $langcode, $items, &$errors);
+    dpm($errors);
+    foreach($items as $delta => $target) {
+      if (isset($errors[$field_item_name][$langcode][$delta])) {
+        unset($results[$entity_type][$target['target_id']]);
+      }
+    }
     if (!empty($results[$entity_type])) {
       $entities = entity_load($entity_type, array_keys($results[$entity_type]));
       foreach ($entities as $entity_id => $entity) {
@@ -69,174 +55,7 @@ class EntityReference_SelectionHandler_Merci implements EntityReference_Selectio
         $options[$bundle][$entity_id] = check_plain($this->getLabel($entity));
       }
     }
-
     return $options;
   }
 
-  /**
-   * Implements EntityReferenceHandler::countReferencableEntities().
-   */
-  public function countReferencableEntities($match = NULL, $match_operator = 'CONTAINS') {
-    $query = $this->buildEntityFieldQuery($match, $match_operator);
-    return $query
-      ->count()
-      ->execute();
-  }
-
-  /**
-   * Implements EntityReferenceHandler::validateReferencableEntities().
-   */
-  public function validateReferencableEntities(array $ids) {
-    if ($ids) {
-      $entity_type = $this->field['settings']['target_type'];
-      $query = $this->buildEntityFieldQuery();
-      $query->entityCondition('entity_id', $ids, 'IN');
-      $result = $query->execute();
-      if (!empty($result[$entity_type])) {
-        return array_keys($result[$entity_type]);
-      }
-    }
-
-    return array();
-  }
-
-  /**
-   * Implements EntityReferenceHandler::validateAutocompleteInput().
-   */
-  public function validateAutocompleteInput($input, &$element, &$form_state, $form) {
-      $entities = $this->getReferencableEntities($input, '=', 6);
-      if (empty($entities)) {
-        // Error if there are no entities available for a required field.
-        form_error($element, t('There are no entities matching "%value"', array('%value' => $input)));
-      }
-      elseif (count($entities) > 5) {
-        // Error if there are more than 5 matching entities.
-        form_error($element, t('Many entities are called %value. Specify the one you want by appending the id in parentheses, like "@value (@id)"', array(
-          '%value' => $input,
-          '@value' => $input,
-          '@id' => key($entities),
-        )));
-      }
-      elseif (count($entities) > 1) {
-        // More helpful error if there are only a few matching entities.
-        $multiples = array();
-        foreach ($entities as $id => $name) {
-          $multiples[] = $name . ' (' . $id . ')';
-        }
-        form_error($element, t('Multiple entities match this reference; "%multiple"', array('%multiple' => implode('", "', $multiples))));
-      }
-      else {
-        // Take the one and only matching entity.
-        return key($entities);
-      }
-  }
-
-  /**
-   * Build an EntityFieldQuery to get referencable entities.
-   */
-  protected function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    $query = new EntityFieldQuery();
-    $query->entityCondition('entity_type', $this->field['settings']['target_type']);
-    if (!empty($this->field['settings']['handler_settings']['target_bundles'])) {
-      $query->entityCondition('bundle', $this->field['settings']['handler_settings']['target_bundles'], 'IN');
-    }
-    if (isset($match)) {
-      $entity_info = entity_get_info($this->field['settings']['target_type']);
-      if (isset($entity_info['entity keys']['label'])) {
-        $query->propertyCondition($entity_info['entity keys']['label'], $match, $match_operator);
-      }
-    }
-
-    // Add a generic entity access tag to the query.
-    $query->addTag($this->field['settings']['target_type'] . '_access');
-    $query->addTag('entityreference');
-    $query->addMetaData('field', $this->field);
-    $query->addMetaData('entityreference_selection_handler', $this);
-
-    // Add the sort option.
-    if (!empty($this->field['settings']['handler_settings']['sort'])) {
-      $sort_settings = $this->field['settings']['handler_settings']['sort'];
-      if ($sort_settings['type'] == 'property') {
-        $query->propertyOrderBy($sort_settings['property'], $sort_settings['direction']);
-      }
-      elseif ($sort_settings['type'] == 'field') {
-        list($field, $column) = explode(':', $sort_settings['field'], 2);
-        $query->fieldOrderBy($field, $column, $sort_settings['direction']);
-      }
-    }
-
-    return $query;
-  }
-
-  /**
-   * Implements EntityReferenceHandler::entityFieldQueryAlter().
-   */
-  public function entityFieldQueryAlter(SelectQueryInterface $query) {
-
-  }
-
-  /**
-   * Helper method: pass a query to the alteration system again.
-   *
-   * This allow Entity Reference to add a tag to an existing query, to ask
-   * access control mechanisms to alter it again.
-   */
-  protected function reAlterQuery(SelectQueryInterface $query, $tag, $base_table) {
-    // Save the old tags and metadata.
-    // For some reason, those are public.
-    $old_tags = $query->alterTags;
-    $old_metadata = $query->alterMetaData;
-
-    $query->alterTags = array($tag => TRUE);
-    $query->alterMetaData['base_table'] = $base_table;
-    drupal_alter(array('query', 'query_' . $tag), $query);
-
-    // Restore the tags and metadata.
-    $query->alterTags = $old_tags;
-    $query->alterMetaData = $old_metadata;
-  }
-
-  /**
-   * Implements EntityReferenceHandler::getLabel().
-   */
-  public function getLabel($entity) {
-    return entity_label($this->field['settings']['target_type'], $entity);
-  }
-
-  /**
-   * Ensure a base table exists for the query.
-   *
-   * If we have a field-only query, we want to assure we have a base-table
-   * so we can later alter the query in entityFieldQueryAlter().
-   *
-   * @param $query
-   *   The Select query.
-   *
-   * @return
-   *   The alias of the base-table.
-   */
-  public function ensureBaseTable(SelectQueryInterface $query) {
-    $tables = $query->getTables();
-
-    // Check the current base table.
-    foreach ($tables as $table) {
-      if (empty($table['join'])) {
-        $alias = $table['alias'];
-        break;
-      }
-    }
-
-    if (strpos($alias, 'field_data_') !== 0) {
-      // The existing base-table is the correct one.
-      return $alias;
-    }
-
-    // Join the known base-table.
-    $target_type = $this->field['settings']['target_type'];
-    $entity_info = entity_get_info($target_type);
-    $id = $entity_info['entity keys']['id'];
-    // Return the alias of the table.
-    return $query->innerJoin($target_type, NULL, "$target_type.$id = $alias.entity_id");
-  }
 }
-
